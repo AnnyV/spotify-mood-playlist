406b809a485f6ba396919be8e93c795aa9a96808
Annyv <annybella18@gmail.com>
2017-01-13 20:27:39 -0500

    merge project

--- assets/js/app.js
+++ assets/js/app.js
@@ -221,3 +221,36 @@ $(document).ready(
         }
 
     });
+
+
+
+    $('#nowfeels').on('click', function(event) {
+
+        var nFeels = $('#iFeel').val().trim();
+
+        event.preventDefault(); 
+
+        console.log(nFeels); 
+
+        // Call YouTube API and create Iframe 
+       
+
+        // Load the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+      height: '360',
+      width: '640',
+      videoId: 'M7lc1UVf-VE'
+    });
+  }
+
+
+    }); 
